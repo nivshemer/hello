417eae3149ceba3925da303002f8e5db6ab2760e
nivshemer <85192571+nivshemer@users.noreply.github.com>
2021-11-21 09:09:23 +0200

    Update hello.sh

--- hello.sh
+++ hello.sh
@@ -4,7 +4,7 @@
 SCRIPT_PATH="$(cd $(dirname ${0}) && pwd)"
 
 FILE="${SCRIPT_PATH}/test.txt"
-# This function wites the message to a file passed as the second argument
+# This function writes the message to a file passed as the second argument
 write_to_file () {
 	local text="${1}"
 	local DEST="${2}"
